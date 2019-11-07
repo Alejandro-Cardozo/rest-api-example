a68b205a0ec1334361d4b6e58c5b2bb9fff9dfa5
Alejandro Cardozo <uri.cardozo@gmail.com>
2019-11-07 17:28:19 -0300

    CRUD routes created and tested

--- src/routes/employees.js
+++ src/routes/employees.js
@@ -12,4 +12,53 @@ router.get('/', (req, res) => {
 	});
 });
 
+router.get('/:id',(req, res) => {
+	var {id} = req.params;
+	mysqlConnection.query('SELECT * FROM employees WHERE id = ?', [id], (err, rows, fields)=>{
+		if(!err){
+			res.json(rows[0]);
+		} else {
+			console.log(err);
+		}
+	});
+});
+
+router.post('/',(req, res) => {
+	const {id, name, salary} = req.body;
+	const query = `
+		CALL addOrEditEmployee(?,?,?);
+	`;
+	mysqlConnection.query(query, [id,name,salary], (err,rows,fields)=>{
+		if(!err){
+			res.json({Status: 'Employeed updated'});
+		} else {
+			console.log(err);
+		}
+	});
+});
+
+router.put('/:id', (req, res) => {
+	const { name, salary} = req.body;
+	const { id } = req.params;
+	const query = 'CALL addOrEditEmployee(?,?,?)';
+	mysqlConnection.query(query, [id, name, salary], (err, rows, fields) => {
+		if(!err){
+			res.json({status: 'Employee updated'});
+		} else {
+			console.log(err);
+		}
+	});
+});
+
+router.delete('/:id', (req, res) => {
+	const { id } = req.params;
+	mysqlConnection.query('DELETE FROM employees WHERE id = ?', [id], (err,rows,fields)=>{
+		if(!err){
+			res.json({status: 'Employee deleted'});
+		} else {
+			console.log(err);
+		}
+	});
+});
+
 module.exports = router;
